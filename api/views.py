5ab3f5dc87b8bdd4ed2c1f9bc57b7002803e9f27
hlyamhtetkyaw <hlan1559@gmail.com>
2025-11-16 14:21:47 +0630

    [ADD] get by id route

--- api/views.py
+++ api/views.py
@@ -24,3 +24,14 @@ def studentsView(request):
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     return Response({'message':'Method not allowed'},status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+@api_view(['GET'])
+def studentsDetailView(request,pk):
+    try:
+        student = Student.objects.get(pk=pk)
+    except Student.DoesNotExist:
+        return Response({'message':'Student not found'},status=status.HTTP_404_NOT_FOUND)
+    
+    if(request.method == 'GET'):
+        serializer = StudentSerializer(student)
+        return Response(serializer.data,status=status.HTTP_200_OK)
