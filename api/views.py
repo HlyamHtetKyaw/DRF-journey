f15cb8be5d482686084f45d286a5b000dcbf554d
hlyamhtetkyaw <hlan1559@gmail.com>
2025-11-16 14:04:09 +0630

    [ADD] serializer

--- api/views.py
+++ api/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+# from django.http import JsonResponse
 from students.models import Student
+from .serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 # Create your views here.
+
+@api_view(['GET'])
 def studentsView(request):
+    # manual serialization
+    # students = Student.objects.all()
+    # student_list = list(students.values())
+    # return JsonResponse(student_list,safe=False)
     students = Student.objects.all()
-    student_list = list(students.values())
-    return JsonResponse(student_list,safe=False)
+    serializer = StudentSerializer(students,many=True)
+    return Response(serializer.data,status=status.HTTP_200_OK)
