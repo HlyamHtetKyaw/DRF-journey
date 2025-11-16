2cbd53f73115fbb4da634e42df0447ec5f9672be
hlyamhtetkyaw <hlan1559@gmail.com>
2025-11-16 14:13:29 +0630

    [ADD] post route for students

--- api/views.py
+++ api/views.py
@@ -7,12 +7,20 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 # Create your views here.
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def studentsView(request):
     # manual serialization
     # students = Student.objects.all()
     # student_list = list(students.values())
     # return JsonResponse(student_list,safe=False)
-    students = Student.objects.all()
-    serializer = StudentSerializer(students,many=True)
-    return Response(serializer.data,status=status.HTTP_200_OK)
+    if(request.method == 'GET'):
+        students = Student.objects.all()
+        serializer = StudentSerializer(students,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    elif(request.method == 'POST'):
+        serializer = StudentSerializer(data=request.data)
+        if(serializer.is_valid()):
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    return Response({'message':'Method not allowed'},status=status.HTTP_405_METHOD_NOT_ALLOWED)
